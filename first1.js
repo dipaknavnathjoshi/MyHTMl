29ec4bf76313462447fcceb77345932ae3d1e7af
Dipak Joshi <joshidipak8010@gmail.com>
2024-12-30 15:40:13 +0530

    js
    Signed-off-by: Dipak Joshi <joshidipak8010@gmail.com>

---
+++ first1.js
@@ -0,0 +1,230 @@
+// var arr=["deepak","joshi","welcome",]
+// // console.log(arr);
+// const students = [
+//     {
+//       name: "Alice",
+//       subjects: [
+//         { subject: "Math", marks: 85 },
+//         { subject: "Science", marks: 90 },
+//         { subject: "English", marks: 78 },
+//       ],
+//     },
+//     {
+//       name: "Bob",
+//       subjects: [
+//         { subject: "Math", marks: 65 },
+//         { subject: "Science", marks: 75 },
+//         { subject: "English", marks: 80 },
+//       ],
+//     },
+//     {
+//       name: "Charlie",
+//       subjects: [
+//         { subject: "Math", marks: 95 },
+//         { subject: "Science", marks: 85 },
+//         { subject: "English", marks: 92 },
+//       ],
+//     },
+//     {
+//       name: "Diana",
+//       subjects: [
+//         { subject: "Math", marks: 78 },
+//         { subject: "Science", marks: 88 },
+//         { subject: "English", marks: 84 },
+//       ],
+//     },
+//     {
+//       name: "Ethan",
+//       subjects: [
+//         { subject: "Math", marks: 80 },
+//         { subject: "Science", marks: 70 },
+//         { subject: "English", marks: 75 },
+//       ],
+//     },
+//     {
+//       name: "Fiona",
+//       subjects: [
+//         { subject: "Math", marks: 88 },
+//         { subject: "Science", marks: 92 },
+//         { subject: "English", marks: 81 },
+//       ],
+//     },
+//     {
+//       name: "George",
+//       subjects: [
+//         { subject: "Math", marks: 60 },
+//         { subject: "Science", marks: 65 },
+//         { subject: "English", marks: 70 },
+//       ],
+//     },
+//     {
+//       name: "Hannah",
+//       subjects: [
+//         { subject: "Math", marks: 90 },
+//         { subject: "Science", marks: 94 },
+//         { subject: "English", marks: 88 },
+//       ],
+//     },
+//     {
+//       name: "Ian",
+//       subjects: [
+//         { subject: "Math", marks: 75 },
+//         { subject: "Science", marks: 80 },
+//         { subject: "English", marks: 85 },
+//       ],
+//     },
+//     {
+//       name: "Julia",
+//       subjects: [
+//         { subject: "Math", marks: 85 },
+//         { subject: "Science", marks: 89 },
+//         { subject: "English", marks: 82 },
+//       ],
+//     },
+//   ];
+ 
+ 
+// console.log(students[0].name);
+// console.log("Math : " + students[0].subjects[0].marks + " Science: " + students[0].subjects[1].marks + " English :" + students[0].subjects[2].marks);
+// var percentage=( students[0].subjects[0].marks+students[0].subjects[1].marks+students[0].subjects[2].marks);
+// console.log(percentage /3);
+// console.log("\n")
+// console.log(students[1].name);
+// console.log( "Math : " + students[1].subjects[0].marks + " Science: " + students[1].subjects[1].marks + " English :" + students[1].subjects[2].marks);
+// var percentage=( students[1].subjects[0].marks+students[1].subjects[1].marks+students[1].subjects[2].marks);
+// console.log(percentage /3);
+// console.log("\n")
+
+// console.log(students[2].name);
+// console.log( "Math : " + students[2].subjects[0].marks + " Science: " + students[2].subjects[1].marks + " English :" + students[2].subjects[2].marks);
+// var percentage=( students[2].subjects[0].marks+students[2].subjects[1].marks+students[2].subjects[2].marks);
+// console.log(percentage /3);
+// console.log("\n")
+
+// console.log(students[3].name);
+// console.log( "Math : " + students[3].subjects[0].marks + " Science: " + students[3].subjects[1].marks + " English :" + students[3].subjects[2].marks);
+// var percentage=( students[3].subjects[0].marks+students[3].subjects[1].marks+students[3].subjects[2].marks);
+// console.log(percentage /3);
+// console.log("\n")
+
+// console.log(students[4].name);
+// console.log( "Math : " + students[4].subjects[0].marks + " Science: " + students[4].subjects[1].marks + " English :" + students[4].subjects[2].marks);
+// var percentage=( students[4].subjects[0].marks+students[4].subjects[1].marks+students[4].subjects[2].marks);
+// console.log(percentage /3);
+// console.log("\n")
+
+// console.log(students[5].name);
+// console.log( "Math : " + students[5].subjects[0].marks + " Science: " + students[5].subjects[1].marks + " English :" + students[5].subjects[2].marks);
+// var percentage=( students[5].subjects[0].marks+students[5].subjects[1].marks+students[5].subjects[2].marks);
+// console.log(percentage /3);
+// console.log("\n")
+
+// console.log(students[6].name);
+// console.log( "Math : " + students[6].subjects[0].marks + " Science: " + students[6].subjects[1].marks + " English :" + students[6].subjects[2].marks);
+// var percentage=( students[6].subjects[0].marks+students[6].subjects[1].marks+students[6].subjects[2].marks);
+// console.log(percentage /3);
+// console.log("\n")
+
+// console.log(students[7].name);
+// console.log( "Math : " + students[7].subjects[0].marks + " Science: " + students[7].subjects[1].marks + " English :" + students[7].subjects[2].marks);
+// var percentage=( students[7].subjects[0].marks+students[7].subjects[1].marks+students[7].subjects[2].marks);
+// console.log(percentage /3);
+// console.log("\n")
+
+// console.log(students[8].name);
+// console.log( "Math : " + students[8].subjects[0].marks + " Science: " + students[8].subjects[1].marks + " English :" + students[8].subjects[2].marks);
+// var percentage=( students[8].subjects[0].marks+students[8].subjects[1].marks+students[8].subjects[2].marks);
+// console.log(percentage /3);
+// console.log("\n")
+
+// console.log(students[9].name);
+// console.log( "Math : " + students[9].subjects[0].marks + " Science: " + students[9].subjects[1].marks + " English :" + students[9].subjects[2].marks);
+// var percentage=( students[9].subjects[0].marks+students[9].subjects[1].marks+students[9].subjects[2].marks);
+// console.log(percentage /3);
+// console.log("\n")
+
+
+var products=[
+    {
+        Name:"Watch",
+        Price:2999,
+        Mrp:5999,
+        Offers:"UPI payments Up to 60% off"
+    }
+]
+// console.log(products[0]);
+
+
+
+var  product=[
+    {
+        name:"Minimalist Dull Skin, Dark Spots & Uneven Tone Treatment 10% Vitamin C Face Serum | Formulated & Tested For Sensitive Skin with Ethyl Ascorbic Acid & PHA | All Skin Types| Brightening & Hydrating Formula for Men & Women | 30ml",
+        brand_name:"Minimalist Store",
+        Ratings:4.1,
+        rating_count:14949,
+        Price:699,
+        Offers:[
+            {
+                Offer_name:"bank offers",
+                About:"Upto ₹69.90 discount on Feder…"
+            },
+            {
+                Offer_name2:"Partner Offers",
+                About2:"2% off any 2, 5% off any 3, 8% off any 4"
+            }
+        ],
+        Discription:[
+            { feature_title:"Skin ",
+                Discription:"Type"
+            },
+            {
+                feature_title:" Product Benefits",
+                Discription:"Brightening" 
+            },
+            { 
+                feature_title :"Use for",
+                Discription:"Face"  
+            },
+            { 
+                feature_title:"Scent",
+                Discription:"Fragrance Free" 
+             },
+            { 
+                feature_title: "Specia",
+                Discription:"Vitamin C"
+            },
+            {  
+                feature_title:"Ingredients"
+                 
+            },
+            {
+                feature_title:"Brand",
+                Discription:"Minimalist"
+            },
+        ],
+        About:[
+             
+            {
+                title_name:"About this item",
+                1:"A POTENT STABLE VITAMIN C FACE SERUM: Not all Vit C serums are made equal! This serum is is made with a stable Vitamin C derivative, 10% Ethyl Ascorbic Acid that is closest to pure Vitamin C (L-Ascorbic Acid) in molecular size and weight. With similar molecule size and 86% pure Vitamin C content it delivers maximum benefits of Vitamin C without getting bad or losing efficacy till the last drop (unlike other Vitamin C serums).",
+                2:"NATURAL GLOW BOOSTER: This glow serum ensures high delivery of Vit C directly into your skin. Vit C reduces melanin production resulting glowing skin. Reduces dullness & tanning and protects from environmental stress such as pollution & sun damage.",
+                3:"SOOTHING & HYDRATING: Formulated with Centella water that soothes and calms skin making this formula non-irritating and suitable for all skin types. 1% Acetyl Glucosamine added in this serum boosts your hydration and natural exfoliation for a plum radiant skin.",
+                4:"CLEAN & TRANSPARENT BEAUTY: This serum is (i) Fragrance-free (ii) Silicones-free (iii) Sulfates-free (iv) Parabens-free (v) Essential Oils-free and (vi) Dyes-free. Also, it is non-comedogenic, oil-free, and hypoallergenic. Formulated at pH 3.9 - 4.9 formulated with high grade COS-VCE (Ethyl Ascorbic Acid) by CosMol Korea, a leading global supplier.",
+                5:"FOR ALL SKIN TYPES: This serum is suitable for all skin types (Dry, Oily or Normal). Formulated for both men and women. With anti-inflammatory Centella water it is suitable for even sensitive skin."
+            }
+        ]
+
+    }
+]
+console.log(product[0]);
+
+
+
+
+
+
+
+
+
+
+ 
